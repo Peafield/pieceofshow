89b4c2b2f52523d4a0fc0292fa5a7ac26b3927b6
peafield <peterjohncoles@gmail.com>
2025-03-07 14:56:56 +0000

    Add Nunito font and restructure layout with new Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<section className="absolute inset-0 flex items-center justify-center">
+			<h1 className="font-sans text-2xl">Hello World!</h1>
+		</section>
+	);
+};
+
+export default Home;
